76400073fc390af61bb46208895f83fab4b102c8
Yuri Konkov <konkov96@gmail.com>
2022-02-19 20:42:40 +0300

    local

--- src/logic/client.js
+++ src/logic/client.js
@@ -1,5 +1,7 @@
+var localUrl = ""
+
 function GetStatus(callback) {
-    const url = process.env.API_URL || "https://hmapi.konkov.xyz/statistics";
+    const url = localUrl || "https://hmapi.konkov.xyz/statistics";
     const options = {
         headers: {
             accept: "application/json"
